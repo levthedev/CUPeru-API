a0b4849e00604d5a04f31d4b58b1a625b31b9238
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 19:41:43 -0600

    fix v1/arity error

--- web/views/v1/message_view.ex
+++ web/views/v1/message_view.ex
@@ -2,11 +2,11 @@ defmodule RestApi.V1.MessageView do
   use RestApi.Web, :view
 
   def render("index.json", %{messages: messages}) do
-    %{data: render_many(messages, RestApi.MessageView, "message.json")}
+    %{data: render_many(messages, RestApi.V1.MessageView, "message.json")}
   end
 
   def render("show.json", %{message: message}) do
-    %{data: render_one(message, RestApi.MessageView, "message.json")}
+    %{data: render_one(message, RestApi.V1.MessageView, "message.json")}
   end
 
   def render("message.json", %{message: message}) do
