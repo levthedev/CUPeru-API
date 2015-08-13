3e3ff66b7be39e2d5d71e7877a2f22ef5a918185
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 19:04:48 -0600

    change post to message in router

--- web/router.ex
+++ web/router.ex
@@ -9,7 +9,7 @@ defmodule RestApi.Router do
     pipe_through :api
 
     scope "/v1", V1, as: :v1 do
-      resources "/posts", PostController
+      resources "/messages", MessageController
     end
   end
 end
