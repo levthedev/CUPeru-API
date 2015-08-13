db6225179b4cd476c9d4637889ccc96c2005a0b0
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 19:03:18 -0600

    versioning test helpers

--- test/controllers/v1/message_controller_test.exs
+++ test/controllers/v1/message_controller_test.exs
@@ -11,13 +11,13 @@ defmodule RestApi.V1.MessageControllerTest do
   end
 
   test "lists all entries on index", %{conn: conn} do
-    conn = get conn, message_path(conn, :index)
+    conn = get conn, v1_message_path(conn, :index)
     assert json_response(conn, 200)["data"] == []
   end
 
   test "shows chosen resource", %{conn: conn} do
     message = Repo.insert! %Message{}
-    conn = get conn, message_path(conn, :show, message)
+    conn = get conn, v1_message_path(conn, :show, message)
     assert json_response(conn, 200)["data"] == %{
       "id" => message.id
     }
@@ -25,37 +25,37 @@ defmodule RestApi.V1.MessageControllerTest do
 
   test "does not show resource and instead throw error when id is nonexistent", %{conn: conn} do
     assert_raise Ecto.NoResultsError, fn ->
-      get conn, message_path(conn, :show, -1)
+      get conn, v1_message_path(conn, :show, -1)
     end
   end
 
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, message_path(conn, :create), message: @valid_attrs
+    conn = post conn, v1_message_path(conn, :create), message: @valid_attrs
     assert json_response(conn, 201)["data"]["id"]
     assert Repo.get_by(Message, @valid_attrs)
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, message_path(conn, :create), message: @invalid_attrs
+    conn = post conn, v1_message_path(conn, :create), message: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "updates and renders chosen resource when data is valid", %{conn: conn} do
     message = Repo.insert! %Message{}
-    conn = put conn, message_path(conn, :update, message), message: @valid_attrs
+    conn = put conn, v1_message_path(conn, :update, message), message: @valid_attrs
     assert json_response(conn, 200)["data"]["id"]
     assert Repo.get_by(Message, @valid_attrs)
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
     message = Repo.insert! %Message{}
-    conn = put conn, message_path(conn, :update, message), message: @invalid_attrs
+    conn = put conn, v1_message_path(conn, :update, message), message: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "deletes chosen resource", %{conn: conn} do
     message = Repo.insert! %Message{}
-    conn = delete conn, message_path(conn, :delete, message)
+    conn = delete conn, v1_message_path(conn, :delete, message)
     assert response(conn, 204)
     refute Repo.get(Message, message.id)
   end
